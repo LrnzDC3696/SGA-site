5a7b2c7979bcc12a107162f064cda670dce79dc2
LrnzdDC3696 <lrnzdc3696@outlook.com>
2022-09-25 09:57:42 +0800

    Added models and changed url directories

--- main/models.py
+++ main/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+YEAR_IN_SCHOOL_CHOICES = [
+    ("4th", "4th Year College"),
+    ("3rd", "3th Year College"),
+    ("2nd", "2th Year College"),
+    ("1st", "1th Year College"),
+    ("12 SHS", "12 Senior High School"),
+    ("11 SHS", "11 Senior High School"),
+]
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length = 69)
+    middle_name = models.CharField(max_length = 69, blank=True, null=True)
+    last_name = models.CharField(max_length = 69)
+    description = models.TextField()
+    email = models.EmailField(max_length = 254)  # 254 is standard maximum string length
+
+    # https://www.geeksforgeeks.org/datetimefield-django-forms/#:~:text=DateTimeField%20in%20Django%20Forms%20is,date%20and%20time%20from%20user.
+    birthday = models.DateField()
+
+    # location?
+    # roles?
+    # major?
+    # minor?
+    # achievements?
+
+    def __str__(self):
+        return self.full_name
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.middle_name[0]} {self.last_name}"
+
+    @property
+    def age(self):
+        # https://stackoverflow.com/a/9754466
+        born = self.birthday
+        today = date.today()
+        return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+
+    def __str__(self):
+        return self.full_name
+
+
+class Role(models.Model):
+    pass
--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="main-index"),
-    path('about/', views.about, name="main-about"),
     path('contact/', views.contact, name="main-contact"),
-    path('sga_officials/', views.sga_officials, name="main-sga_officials"),
-    path('batch_governors/', views.batch_governors, name="main-batch_governors"),
+    path('about/', views.about, name="main-about"),
+    path('about/sga_officials/', views.sga_officials, name="main-sga_officials"),
+    path('about/batch_governors/', views.batch_governors, name="main-batch_governors"),
 ]
